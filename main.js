c5724a3bc4f7709a78dc4f0e65143a30487e1e54
Manoel Prado <manoelprado.aecjr@gmail.com>
2021-08-01 18:27:51 -0300

    add a new carousel

--- main.js
+++ main.js
@@ -205,3 +205,24 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+/* SHOW IMAGES #ABOUT - CAROUSEL 2 #ABOUT*/
+var slideIndex2 = 0;
+showSlides2();
+
+function showSlides2() {
+  var i2;
+  var slides2 = document.getElementsByClassName("mySlides2");
+  var dots2 = document.getElementsByClassName("dot2");
+  for (i2 = 0; i2 < slides2.length; i2++) {
+    slides2[i2].style.display = "none";  
+  }
+  slideIndex2++;
+  if (slideIndex2 > slides2.length) {slideIndex2 = 1}    
+  for (i2 = 0; i2 < dots2.length; i2++) {
+    dots2[i2].className = dots2[i2].className.replace(" active", "");
+  }
+  slides2[slideIndex2-1].style.display = "block";  
+  dots2[slideIndex2-1].className += " active";
+  setTimeout(showSlides2, 3000); // Change image every 3 seconds
+}
